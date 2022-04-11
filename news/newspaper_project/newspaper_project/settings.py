26624304cc495a59b620bd8810e8e02bedd70a2a
arugenre <200103185@stu.sdu.edu.kz>
2022-04-11 15:18:19 +0600

    ignore

--- news/newspaper_project/newspaper_project/settings.py
+++ news/newspaper_project/newspaper_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from . import secret
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8p103e8qh^602avi7+n$zb+2xl8$b)o#m-t7w+ol8#85i%9k^u'
+SECRET_KEY = secret.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

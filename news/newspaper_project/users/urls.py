b7643bb9cd47efa16f4c9b24b514c610148182ca
arugenre <200103185@stu.sdu.edu.kz>
2022-04-22 11:51:05 +0600

    added UserDetailView, new url for user_detail

--- news/newspaper_project/users/urls.py
+++ news/newspaper_project/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.views.generic.base import TemplateView # new
-from .views import SignUpView
+from .views import SignUpView, UserDetailView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name = 'signup'),
+    path('<int:pk>/', UserDetailView.as_view(), name = 'user_detail'),
 ]
--- news/newspaper_project/users/views.py
+++ news/newspaper_project/users/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -9,3 +10,8 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
     
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = CustomUser
+    login_url = 'login'
+    template_name = 'user_detail.html'
+
